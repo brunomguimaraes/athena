9c0b434af8914e3481eed3e1486e457d95500c9d
Bruno <mguimaraesbruno@gmail.com>
2022-07-04 19:45:50 -0300

    add password to user

--- api/models/User.go
+++ api/models/User.go
@@ -1,14 +1,38 @@
 package models
 
 import (
+	"errors"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
 type User struct {
-	UserID        int    `gorm:"primary_key;auto_increment" json:"id"`
-	UserFirstName string `gorm:"size:255;not null;" json:"firstname"`
-	UserLastName  string `gorm:"size:255;not null;" json:"lastname"`
-	UserEmail     string `gorm:"size:100;not null;unique" json:"email"`
+	UserID        int       `gorm:"primary_key;auto_increment" json:"id"`
+	UserFirstName string    `gorm:"size:255;not null;" json:"firstname"`
+	UserLastName  string    `gorm:"size:255;not null;" json:"lastname"`
+	UserEmail     string    `gorm:"size:100;not null;unique" json:"email"`
+	Password      string    `gorm:"size:100;not null;" json:"password"`
+	CreatedAt     time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt     time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
+}
+
+func Hash(password string) ([]byte, error) {
+	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+}
+
+func VerifyPassword(hashedPassword, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
+
+func (u *User) SecurePassword() error {
+	hashedPassword, err := Hash(u.Password)
+	if err != nil {
+		return err
+	}
+	u.Password = string(hashedPassword)
+	return nil
 }
 
 func (u *User) FindAllUsers(db *gorm.DB) (*[]User, error) {
@@ -20,4 +44,26 @@ func (u *User) FindAllUsers(db *gorm.DB) (*[]User, error) {
 		return &[]User{}, err
 	}
 	return &users, err
+}
+
+func (u *User) SaveUser(db *gorm.DB) (*User, error) {
+
+	var err error
+	err = db.Debug().Create(&u).Error
+	if err != nil {
+		return &User{}, err
+	}
+	return u, nil
+}
+
+func (u *User) FindUserByID(db *gorm.DB, uid uint32) (*User, error) {
+	var err error
+	err = db.Debug().Model(User{}).Where("id = ?", uid).Take(&u).Error
+	if err != nil {
+		return &User{}, err
+	}
+	if errors.Is(db.Error, gorm.ErrRecordNotFound) {
+		return &User{}, errors.New("User Not Found")
+	}
+	return u, err
 }
