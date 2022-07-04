574587360cc93796f83e9e4439223225096b6b4f
Bruno <mguimaraesbruno@gmail.com>
2022-07-04 13:06:58 -0300

    removes debug logs

--- api/controllers/users_controller.go
+++ api/controllers/users_controller.go
@@ -3,12 +3,10 @@ package controllers
 import (
 	"athena/api/models"
 	"athena/api/responses"
-	"fmt"
 	"net/http"
 )
 
 func (server *Server) GetUsers(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("called GetUsers")
 	user := models.User{}
 
 	users, err := user.FindAllUsers(server.DB)
