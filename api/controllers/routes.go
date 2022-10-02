9041d55654760d4f81ebf24a6755db7e3c134f30
Bruno <mguimaraesbruno@gmail.com>
2022-10-02 12:02:35 -0300

    Renames some models variables

--- api/controllers/routes.go
+++ api/controllers/routes.go
@@ -9,6 +9,9 @@ func (server *Server) initializeRoutes() {
 	//Users routes
 	server.Router.HandleFunc("/users", middlewares.SetMiddlewareJSON(server.CreateUser)).Methods("POST")
 	server.Router.HandleFunc("/users", middlewares.SetMiddlewareJSON(server.GetUsers)).Methods("GET")
+	server.Router.HandleFunc("/users/{id}", middlewares.SetMiddlewareJSON(server.GetUser)).Methods("GET")
+	// server.Router.HandleFunc("/users/{id}", middlewares.SetMiddlewareJSON(middlewares.SetMiddlewareAuthentication(server.UpdateUser))).Methods("PUT")
+	// server.Router.HandleFunc("/users/{id}", middlewares.SetMiddlewareJSON(server.DeleteUser)).Methods("DELETE")
 
 	//Auth routes
 	server.Router.HandleFunc("/auth/login", middlewares.SetMiddlewareJSON(server.Login)).Methods("POST")
